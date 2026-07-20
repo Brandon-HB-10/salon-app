ecf786d4969dff66461316a90f99deffaa6f46e9
Bran.HB <brandonhernandezbriones456@gmail.com>
2026-07-20 01:35:42 -0600

    agrege mi url de mi backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://salon-app-backend-6fv2.onrender.com',
 })
 
 api.interceptors.request.use((config) => {
